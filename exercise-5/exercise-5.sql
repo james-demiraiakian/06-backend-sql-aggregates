9a58895f96cc4a056bc8236737995adef7c7b807
James Demiraiakian <james.armen@gmail.com>
2022-03-23 14:35:04 -0700

    fin 5

--- exercise-5/exercise-5.sql
+++ exercise-5/exercise-5.sql
@@ -1,1 +1,10 @@
 -- the total run time of all inventory items if they were all played back to back
+
+SELECT
+	SUM (length) AS sum
+FROM
+	film
+RIGHT JOIN
+	inventory
+ON
+	film.film_id = inventory.film_id
